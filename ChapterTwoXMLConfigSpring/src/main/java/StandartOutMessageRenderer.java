d44cc1b62fc954646d98d777353253f50663605f
Tanrari <xxxvalvexxx@mail.ru>
2023-02-01 16:11:48 +0300

    first

---
+++ ChapterTwoXMLConfigSpring/src/main/java/StandartOutMessageRenderer.java
@@ -0,0 +1,18 @@
+public class StandartOutMessageRenderer implements MessageRenderer {
+    private MessageProvider messageProvider;
+    public void render() {
+        if (messageProvider==null) {
+            throw new RuntimeException("You must set the property messageProvider of class: " +
+                    StandartOutMessageRenderer.class.getName());
+        }
+        System.out.println(messageProvider.getMessage());
+    }
+
+    public void setMessageProvider(MessageProvider provider) {
+        this.messageProvider=provider;
+    }
+
+    public MessageProvider getMessageProvider() {
+        return this.messageProvider;
+    }
+}
